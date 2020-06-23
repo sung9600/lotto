543ff3fd9be7b31acd28ac8f0c7724feb4a41916
sung9600 <sung96002@naver.com>
2020-06-23 17:20:50 +0900

    blog1

--- app/src/main/java/edu/skku2/map/lotto/MainActivity.java
+++ app/src/main/java/edu/skku2/map/lotto/MainActivity.java
@@ -1,14 +1,104 @@
 package edu.skku2.map.lotto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.extensions.HdrImageCaptureExtender;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Executor executor= Executors.newSingleThreadExecutor();
+    private int REQUEST_CODE_PERMISSIONS =1001;
+    private final String[] REQUIRED_PERMISSIONS= new String[]{
+            "android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"
+    };
+    PreviewView mPreviewView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPreviewView=findViewById(R.id.previewView);
+        if(allPermissionsGranted()){
+            startCamera();
+        }
+        else{
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
     }
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+    private void startCamera(){
+        final ListenableFuture<ProcessCameraProvider>
+                cameraProviderListenableFuture=ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    ProcessCameraProvider cameraProvider=cameraProviderListenableFuture.get();
+                    bindPreview(cameraProvider);
+                }catch (ExecutionException|InterruptedException ignored){
+
+                }
+            }
+        },ContextCompat.getMainExecutor(this));
+    }
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider){
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector=new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        ImageCapture.Builder builder=new ImageCapture.Builder();
+        ImageAnalysis imageAnalysis=new ImageAnalysis.Builder().build();
+        HdrImageCaptureExtender hdrImageCaptureExtender=HdrImageCaptureExtender.create(builder);
+
+        if(hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)){
+            hdrImageCaptureExtender.enableExtension(cameraSelector);
+
+        }
+        final ImageCapture imageCapture=builder.setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation()).build();
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+        Camera camera= (Camera) cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageAnalysis,imageCapture);
+    }
+
 }
+
+
+
+
+
